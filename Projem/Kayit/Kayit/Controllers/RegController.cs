1a56e4ecf276acb589a83e4d3241cca20553b972
anilorhan97 <anilorhan97@gmail.com>
2022-10-17 21:39:15 +0300

    Kullanıcı Kayıt Ekranı
    .Net MVC ile validation kurallarına uygun kullanıcı kayıt ekranı uygulaması.

---
+++ Projem/Kayit/Kayit/Controllers/RegController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kayit.Models;
+
+namespace Kayit.Controllers
+{
+    public class RegController : Controller
+    {
+        ProjemEntities db = new ProjemEntities();
+        // GET: Reg
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+        [HttpPost]
+        public ActionResult Register(kayit user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+            var yeniKayit = db.kayit.Add(user);
+            db.SaveChanges();
+            return View("Register");
+
+        }
+    }
+}
---
+++ Projem/Kayit/Kayit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kayit.MvcApplication" Language="C#" %>
